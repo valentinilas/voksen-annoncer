a19971148407fe40e005f69b138d32c7779af196
Valentin Ilas <valentin.ilas@gmail.com>
2024-06-04 00:52:18 +0200

    new env

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { loadEnv } from 'vite'
+// import { loadEnv } from 'vite'
 // https://vitejs.dev/config/
 // export default defineConfig({
 //   // base:'/voksen-annoncer/',
@@ -8,12 +8,31 @@ import { loadEnv } from 'vite'
 //   plugins: [react()],
 // })
 
+// export default defineConfig(({ mode }) => {
+//   // Load environment variables based on the mode (development or production)
+//   const env = loadEnv(mode, process.cwd())
+
+//   return {
+//     base: env.VITE_BASE_PATH,  // Set the base path from the environment variable
+//     plugins: [react()],
+//   }
+// })
+
+
+
+import { defineConfig, loadEnv } from 'vite'
+
 export default defineConfig(({ mode }) => {
-  // Load environment variables based on the mode (development or production)
+  // Load environment variables based on the current mode (e.g., development or production)
   const env = loadEnv(mode, process.cwd())
 
   return {
-    base: env.VITE_BASE_PATH,  // Set the base path from the environment variable
+    base: env.VITE_BASE_PATH,
     plugins: [react()],
+    // Add this to make sure environment variables are available to your application
+    define: {
+      'process.env': env
+    }
   }
 })
+
